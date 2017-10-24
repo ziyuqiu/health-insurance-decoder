70ca09b283b6b91aebabd10140e3f9226a679a15
10shibu <shibu@brandeis.edu>
2017-10-24 15:34:19 -0400

    fixed log names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/log'
+  get 'logs/log'
 
   resources :facilities
   resources :plans
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get  '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
-  
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
