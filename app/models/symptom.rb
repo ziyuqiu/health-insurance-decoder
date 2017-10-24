77ff95cb4e3b5e9196ff538bca2ac40f64d4fbf9
amg <amgoncalves@users.noreply.github.com>
2017-10-23 20:54:19 -0400

    Adds symptom checks and unit tests.

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,4 +1,6 @@
 class Symptom < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 75 }, uniqueness: true
+  
   has_many :body_part_symptom_diseases
   has_many :body_parts, :through => :body_part_symptom_diseases
   has_many :diseases, :through => :body_part_symptom_diseases
--- test/models/symptom_test.rb
+++ test/models/symptom_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SymptomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @symptom = Symptom.new(id: 1, name: "Floating one inch off the ground")
+  end
+
+  test "should be valid" do
+    assert @symptom.valid?
+  end
+
+  test "name should be present" do
+    @symptom.name = "    "
+    assert_not @symptom.valid?
+  end
+
+  test "symptom name should not be too long" do
+    @symptom.name = "a" * 76
+    assert_not @symptom.valid?
+  end
+
+  test "symptom name should be unique" do
+    duplicate_symptom = @symptom.dup
+    @symptom.save
+    assert_not duplicate_symptom.valid?
+  end
 end
