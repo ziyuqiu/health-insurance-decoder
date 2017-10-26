175b04d440b8deb2236fda5b4780a8b488239c28
10shibu <shibu@brandeis.edu>
2017-10-26 18:55:25 -0400

    finish user_model_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,17 +10,24 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "should be valid" do
-    assert @user.valid?
+    @user.must_be :valid?
   end
 
   test "name should be present" do
     @user.name = "     "
-    assert_not @user.valid?
+    @user.must_be :present?
+    #@user.errors[:name].must_be :present?
   end
 
   test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
+    #user.errors[:email].must_be :present?
+  end
+
+  test "user email should be unique" do
+    @duplicate_user = User.create(name: "Example User2", email: "user@example.com", password: "123456")
+    @user.email.wont_be_same_as @duplicate_user.email
   end
 
   test "password should be present" do
