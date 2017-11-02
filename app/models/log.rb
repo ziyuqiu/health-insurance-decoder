54d59409c094403619e63f6b2e5c528bedb00def
10shibu <shibu@brandeis.edu>
2017-11-02 16:37:50 -0400

    new calendar look

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,6 @@
 class Log < ApplicationRecord
   belongs_to :symptom
+  def start_time
+        self.created_at ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
+  end
 end
