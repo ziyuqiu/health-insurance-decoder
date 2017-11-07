3be627e3871b5a634715190ff541f324ed8f6f03
ziyuqiu <ziyuqiu@brandeis.edu>
2017-11-07 14:40:29 -0500

    link logs to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031202136) do
+ActiveRecord::Schema.define(version: 20171107192319) do
 
   create_table "body_part_symptom_diseases", force: :cascade do |t|
     t.integer "body_part_id"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20171031202136) do
     t.integer "severity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "plans", force: :cascade do |t|
@@ -112,6 +113,7 @@ ActiveRecord::Schema.define(version: 20171031202136) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "email"
+    t.integer "user_id"
   end
 
 end
