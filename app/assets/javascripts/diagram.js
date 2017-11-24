38f81836349aa32df029324b29f005cdb3798e98
amg <amgoncalves@users.noreply.github.com>
2017-11-24 12:51:05 -0500

    Cleans up javascript, changes order of css loading so bootstrap doesn't override custom css.

--- app/assets/javascripts/diagram.js
+++ app/assets/javascripts/diagram.js
@@ -1,18 +1,40 @@
 console.log("Hello, body diagram.");
-$(document).ready(function(){
+
+var addHighlight = function($button) {
+    $button.addClass('btn_active').removeClass('btn_muted');    
+}
+
+var removeHighlight = function($button) {
+    $button.addClass('btn_muted').removeClass('btn_active');    
+}
+
+var switchImage = function() {
+    var img = $("#body-image");
+    if (img.hasClass('femalebg'))
+	img.removeClass('femalebg').addClass('malebg');
+    else if (img.hasClass('malebg'))
+	img.removeClass('malebg').addClass('femalebg');
+}
+
+var initBodyImageSwitches = function() {
     $("#body-image").addClass('femalebg');
-    $("#btn_m").addClass('btn_active');
-    $("#btn_f").addClass('btn_disabled');
+    $("#btn_m").addClass('btn_muted');
+    $("#btn_f").addClass('btn_active');    
+}
+
+$(document).ready(function(){
     
+    initBodyImageSwitches();
+
     $("#btn_m").click(function(){
-	$("#body-image").addClass('malebg').removeClass('femalebg');
-	$(this).addClass('btn_disabled').removeClass('btn_active');
-	$("#btn_f").addClass('btn_active').removeClass('btn_disabled');	
+	switchImage();
+	addHighlight($(this));
+	removeHighlight($("#btn_f"));
     });
     
     $("#btn_f").click(function(){
-	$("#body-image").addClass('femalebg').removeClass('malebg');
-	$(this).addClass('btn_disabled').removeClass('btn_active');
-	$("#btn_m").addClass('btn_active').removeClass('btn_disabled');
+	switchImage();
+	addHighlight($(this));
+	removeHighlight($("#btn_m"));
     });
 });
