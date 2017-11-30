6ce048797f948f694c3769978b75a8718572f49d
ziyuqiu <ziyuqiu@brandeis.edu>
2017-11-30 15:36:52 -0500

    rss working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def emergency
   end
+  def resources
+		require 'rss'
+		require 'open-uri'
+		rss_results = []
+		@rss = RSS::Parser.parse('http://www.trumba.com/calendars/brandeis-university-health.rss', false)
+		print @rss
+		@rss.items.each do |result|
+		   	result = { title: result.title, link: result.link, description: result.description }
+		    rss_results.push(result)
+		end
+	    @events = rss_results
+	end
 end
