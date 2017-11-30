824b84874d4e6a1bf50fe277f629d2fa27636028
ziyuqiu <ziyuqiu@brandeis.edu>
2017-11-30 16:12:54 -0500

    fix event page showing raw html issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,9 +6,8 @@ class StaticPagesController < ApplicationController
 		require 'open-uri'
 		rss_results = []
 		@rss = RSS::Parser.parse('http://www.trumba.com/calendars/brandeis-university-health.rss', false)
-		print @rss
 		@rss.items.each do |result|
-		   	result = { title: result.title, link: result.link, description: result.description }
+		   	result = { title: result.title, link: result.link, description: result.description, date: result.category }
 		    rss_results.push(result)
 		end
 	    @events = rss_results
