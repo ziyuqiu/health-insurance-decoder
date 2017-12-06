04ed67fdfbf6cb2fcbd202be4288fb38c74f215b
ziyuqiu <ziyuqiu@brandeis.edu>
2017-12-05 20:07:38 -0500

    precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,17 +11,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( slider.js)
-# Rails.application.config.assets.precompile += %w( default_prof_pic.png)
-Rails.application.config.assets.precompile += %w[*.png *.gif *.jpeg]
-# Rails.application.config.assets.precompile += %w( default_prof_pic.png)
-# Rails.application.config.assets.precompile += %w( light-veneer.png)
-# Rails.application.config.assets.precompile += %w(white_plaster.png)
-# Rails.application.config.assets.precompile += %w( pink_rice.png)
 
-Rails.application.config.assets.precompile += %w( pain_chart.gif)
-Rails.application.config.assets.precompile += %w( clinic_lst.js )
-Rails.application.config.assets.precompile += %w( geolocation.js )
-Rails.application.config.assets.precompile += %w( resources.js )
-Rails.application.config.assets.precompile += %w( log_index.js )
+Rails.application.config.assets.precompile += %w[*.png *.gif *.jpeg]
+Rails.application.config.assets.precompile += %w[ *.js ]
