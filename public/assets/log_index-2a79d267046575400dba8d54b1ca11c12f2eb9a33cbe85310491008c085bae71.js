361e4ed288ca949ca7e2432f912bc2df50271a5b
ziyuqiu <ziyuqiu@brandeis.edu>
2017-12-05 20:04:58 -0500

    revert save

---
+++ public/assets/log_index-2a79d267046575400dba8d54b1ca11c12f2eb9a33cbe85310491008c085bae71.js
@@ -0,0 +1,45 @@
+// Get all elements with class = typeName and toggle visibility
+function toggleLog(typeName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName(typeName);
+    for (i = 0; i < tabcontent.length; i++) {
+	if (tabcontent[i].style.display != "block") {
+	    tabcontent[i].style.display = "block";
+	} else if (tabcontent[i].style.display != "none") {
+	    tabcontent[i].style.display = "none";
+	}
+    }
+}
+
+// Opens browser printer dialogue when user clicks on the printer icon in the Log History menu.
+function printSymptomsData(){
+    var divToPrint=document.getElementById("printSymptomsTable");
+    newWin= window.open("");
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.print();
+    newWin.close();
+}
+
+// Opens browser printer dialogue when user clicks on the printer icon in the Log History menu.
+function printVisitsData(){
+    var divToPrint=document.getElementById("printVisitsTable");
+    newWin= window.open("");
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.print();
+    newWin.close();
+}
+
+function executeSymptomSearch() {
+    console.log("Search field changed.");
+    var s= document.getElementById("search_symptoms").value;
+    console.log(s);
+    var params = {
+	symptom_name: s,
+    }
+    $.ajax({
+	url: "/update_logs",
+	type: "POST",
+	data: params
+    });
+}
+;
