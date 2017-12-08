09fc8f41a95b2370d32736460b9e1b00550a1350
amg <amgoncalves@users.noreply.github.com>
2017-12-08 09:14:26 -0500

    Fixes symptom search on logs page.

--- app/assets/javascripts/log_index.js
+++ app/assets/javascripts/log_index.js
@@ -39,6 +39,7 @@ function executeSymptomSearch() {
     $.ajax({
 	url: "/update_logs",
 	type: "POST",
+        beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
 	data: params
     });
 }
