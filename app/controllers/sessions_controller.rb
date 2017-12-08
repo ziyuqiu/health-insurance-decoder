12e07bde318613733f973868fba07ccb4be15f21
10shibu <shibu@brandeis.edu>
2017-12-08 15:17:01 -0500

    routes fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
-
+  $flag = 0
   def new
   end
 
   def create
 
+
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       respond_to do |format|
-        format.html { redirect_to root_url, :flash => { :success => "You are logged in as "+current_user.name } }
+        if $flag==0
+          format.html { redirect_to root_url, :flash => { :success => "You are logged in as "+current_user.name } }
+        else
+          format.html { redirect_to '/logs', :flash => { :success => "You are logged in as "+current_user.name } }
+        end
       end
     else
       flash.now[:danger] = 'Invalid email/password combination'
