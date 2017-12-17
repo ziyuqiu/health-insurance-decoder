2f87f90753cab17614ca5cc84582b7aeb0894a73
amg <amgoncalves@users.noreply.github.com>
2017-12-17 11:11:15 -0500

    Restores and fixes user login, logout tests.

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -15,42 +15,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-=begin
-  #TODO: UPDATE TEST TO REFLECT LATEST CHANGES
   test "login with valid information" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-    assert_redirected_to @user
+    assert_redirected_to root_url
     follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_template 'home/_profile'
+    assert_template 'home/index'
   end  
-=end
 
-=begin
-  # TODO: UPDATE TEST TO REFLECT LATEST CHANGES
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
     assert is_logged_in?
-    assert_redirected_to @user
+    assert_redirected_to root_url
     follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_template 'home/_profile'
+    assert_template 'home/index'
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
-    delete logout_path
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
   end
-=end
 end
