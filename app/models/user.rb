d3cb44654e20decb297e7ede100cfe57501c9584
amg <amgoncalves@users.noreply.github.com>
2017-12-18 10:01:16 -0500

    Fixes typo in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   private
 
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : nBCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
